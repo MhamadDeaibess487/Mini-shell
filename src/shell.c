5f6c620445bbd25b4e5001571a94cc1fd4dcb40d
MhamadDeaibess487 <mohammaddeaibess342@gmail.com>
2026-02-15 17:59:53 +0100

    question 3 les 5 premieres parties

--- src/shell.c
+++ src/shell.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include "readcmd.h"
 #include "csapp.h"
+#include <string.h>
 
 
 int main()
@@ -31,15 +32,65 @@ int main()
 
 		if (l->in) printf("in: %s\n", l->in);
 		if (l->out) printf("out: %s\n", l->out);
-
+		
 		/* Display each command of the pipe */
+	
+		
 		for (i=0; l->seq[i]!=0; i++) {
 			char **cmd = l->seq[i];
 			printf("seq[%d]: ", i);
+			
 			for (j=0; cmd[j]!=0; j++) {
-				printf("%s ", cmd[j]);
+				printf("%s \n", cmd[j]);
+				
+
 			}
-			printf("\n");
 		}
+	
+		
+		if(strcmp(l->seq[0][0], "quit") == 0){
+			printf("caught sigint, exiting\n");
+			exit(0);
+		}
+		else{
+			pid_t pid1 = Fork();
+			
+			if(pid1 == 0){
+				
+				if(l->in!=NULL){
+					int fd0 = open(l->in, O_RDONLY, 0);
+					if(fd0 < 0){
+						fprintf(stderr, "%s: permission denied\n", l->in);
+						exit(0);
+					}
+					Dup2(fd0, 0);
+					Close(fd0);
+					
+				}
+				if(l->out!=NULL){
+					int fd1 = open(l->out, O_WRONLY|O_CREAT|O_TRUNC, DEF_MODE);
+					if(fd1 < 0){
+						fprintf(stderr, "%s: permission denied\n", l->out);
+						exit(0);
+					}
+					Dup2(fd1, 1);
+					Close(fd1);
+				
+
+				}
+				
+				
+				execvp(l->seq[0][0], l->seq[0]);
+				fprintf(stderr, "%s: command not found\n", l->seq[0][0]);
+				exit(0);
+			}
+			else{
+				wait(NULL);
+			}
+			
+		}
+	
+
+		
 	}
 }
